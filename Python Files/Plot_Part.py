47c338dc84e9c6e728a9a658242c1416edd8ab5f
Shakil Imtiaz <shakil.imtiaz@rediffmail.com>
2022-12-14 10:31:24 +0530

    Plot all datasets

--- Python Files/Plot_Part.py
+++ Python Files/Plot_Part.py
@@ -9,12 +9,12 @@ fig = plt.figure(figsize=(6,5))
 left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
 ax = fig.add_axes([left, bottom, width, height])
 
-while k<5000:
+while k<=900:
 	file_name = 'part_%d.txt'%(k)
-	path = './output'
+	path = './output/'
 
 	if os.path.exists(pjoin(path,file_name)):
-		I, x, y = np.loadtxt(pjoin(path,file_name),unpack=True)
+		x, y = np.loadtxt(pjoin(path,file_name),unpack=True)
 	else:
 		print('No Data')
 		exit()
